06996567ce6076a0069d99d6af36db1455520b12
YH-100203 <griezmannlyh1002@gmail.com>
2025-12-08 01:01:10 +0800

    Initial data model implementation

---
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -0,0 +1,99 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue()
+    private long clientId;
+
+    @ManyToOne // Annotation to tell JPA that Many clients belong to One advisor
+    @JoinColumn(name = "advisorId", nullable = false) // Creates a foreign key column to the Advisor table
+    private Advisor advisor;
+
+    @Column(nullable = false) // nullable means you can't create an entry without a value in this column
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String email;
+
+    // Required by JPA
+    protected Client() {
+
+    }
+
+    public Client(Advisor advisor, String firstName, String lastName, String address, String phone, String email) {
+        this.advisor = advisor;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+
+    public Advisor getAdvisor() { return advisor; }
+
+    public void setAdvisor(Advisor advisor) { this.advisor = advisor; }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
